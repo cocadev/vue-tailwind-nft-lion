756a6b62213ca137e0619a28f4ee46ee91f89d5f
scmoo <eugene19950901@outlook.com>
2021-11-08 17:37:52 +0800

    initial commits

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Main />
+    <!-- <Nav class="mb-6" />
+    <HelloWorld msg="Welcome to Your Vue.js App" class="text-center"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+// import Nav from './components/Nav.vue'
+import Main from './pages/main.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Main
+    // HelloWorld,
+    // Nav
+  }
+}
+</script>
---
+++ src/pages/footer.vue
@@ -0,0 +1,189 @@
+<template>
+    <footer
+      class="
+        bg-yellow-300
+        px-4
+        lg:px-12 lg
+        py-8
+        md:py-0
+        relative
+        Home_footer__1WdhD
+      "
+    >
+      <div class="relative z-30 max-w-7xl mx-auto">
+        <div class="flex items-end justify-between">
+          <div class="hidden md:flex">
+            <span
+              style="
+                box-sizing: border-box;
+                display: inline-block;
+                overflow: hidden;
+                width: initial;
+                height: initial;
+                background: none;
+                opacity: 1;
+                border: 0;
+                margin: 0;
+                padding: 0;
+                position: relative;
+                max-width: 100%;
+              "
+              ><span
+                style="
+                  box-sizing: border-box;
+                  display: block;
+                  width: initial;
+                  height: initial;
+                  background: none;
+                  opacity: 1;
+                  border: 0;
+                  margin: 0;
+                  padding: 0;
+                  max-width: 100%;
+                "
+                ><img
+                  style="
+                    display: block;
+                    max-width: 100%;
+                    width: initial;
+                    height: initial;
+                    background: none;
+                    opacity: 1;
+                    border: 0;
+                    margin: 0;
+                    padding: 0;
+                  "
+                  alt=""
+                  aria-hidden="true"
+                  src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDAwIiBoZWlnaHQ9IjQwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=" /></span
+              ><img
+                alt="lion"
+                src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                decoding="async"
+                data-nimg="intrinsic"
+                style="
+                  position: absolute;
+                  top: 0;
+                  left: 0;
+                  bottom: 0;
+                  right: 0;
+                  box-sizing: border-box;
+                  padding: 0;
+                  border: none;
+                  margin: auto;
+                  display: block;
+                  width: 0;
+                  height: 0;
+                  min-width: 100%;
+                  max-width: 100%;
+                  min-height: 100%;
+                  max-height: 100%;
+                " /><noscript
+                ><img
+                  alt="lion"
+                  srcset="
+                    /_next/image?url=%2Flion.png&amp;w=640&amp;q=75 1x,
+                    /_next/image?url=%2Flion.png&amp;w=828&amp;q=75 2x
+                  "
+                  src="_next/lion.png"
+                  decoding="async"
+                  data-nimg="intrinsic"
+                  style="
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    bottom: 0;
+                    right: 0;
+                    box-sizing: border-box;
+                    padding: 0;
+                    border: none;
+                    margin: auto;
+                    display: block;
+                    width: 0;
+                    height: 0;
+                    min-width: 100%;
+                    max-width: 100%;
+                    min-height: 100%;
+                    max-height: 100%;
+                  "
+                  loading="lazy" /></noscript
+            ></span>
+          </div>
+          <div
+            class="
+              bg-gray-500
+              text-white
+              font-serif
+              py-6
+              px-10
+              w-11/12
+              mx-auto
+              md:ml-auto md:mr-0
+              self-center
+              md:w-5/6
+              lg:w-3/6
+            "
+          >
+            <p class="text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-2xl">
+              VERIFIED SMART CONTRACT:
+            </p>
+            <p
+              class="
+                text-lg
+                mx:auto
+                md:text-xl
+                lg:text-2xl
+                xl:text-3xl
+                2xl:text-2xl
+                font-bold
+                text-green-300
+              "
+            >
+              <a
+                href="../etherscan.io/address/0x8943c7bac1914c9a7aba750bf2b6b09fd21037e0.html"
+                >View on Etherscan
+              </a>
+            </p>
+            <div
+              class="w-32 bg-yellow-300 my-6 lg:my-8"
+              style="height: 2px"
+            ></div>
+            <a
+              href="../bcm.media/privacy-policy-lazy-lions/index.html"
+              target="_blank"
+              rel="noreferrer"
+              >Privacy Policy
+            </a>
+            |
+            <a
+              href="../bcm.media/terms-of-sale-lazy-lions/index.html"
+              target="_blank"
+              rel="noreferrer"
+              >Terms of Sale
+            </a>
+          </div>
+        </div>
+      </div>
+      <p
+        class="
+          xl:text-xl
+          md:absolute
+          bottom-2
+          left-1/2
+          transform
+          md:-translate-x-1/2
+          text-white text-center
+          mt-3
+          md:mt-0
+        "
+      >
+        © 2021 Blockchain Media Pty Ltd
+      </p>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
